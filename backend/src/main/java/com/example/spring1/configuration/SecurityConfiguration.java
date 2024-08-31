40ec8b4ae506e9cdd7e991e96f815aa6cc46f24a
praneethls4008 <praneethls4008@gmail.com>
2024-08-31 21:23:55 +0530

    Authentication1

--- backend/src/main/java/com/example/spring1/configuration/SecurityConfiguration.java
+++ backend/src/main/java/com/example/spring1/configuration/SecurityConfiguration.java
@@ -5,18 +5,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-//@EnableWebSecurity
+@EnableWebSecurity
 public class SecurityConfiguration{
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        /*antMatchers("/public/**").permitAll()  // Public endpoints
-        .antMatchers("/private/**").authenticated()*/
-        http.csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(request -> request
-                        .anyRequest().permitAll()).httpBasic(Customizer.withDefaults());
-        return http.build();
+        http.csrf(csrf -> csrf.disable());
+        return http.authorizeHttpRequests(request -> {
+            request.requestMatchers("/user").hasRole("OWNER");
+            request.requestMatchers("/video").hasRole("REGISTERED_USER");
+            request.anyRequest().permitAll();
+        })
+                .build();
+
     }
+
+
 }
